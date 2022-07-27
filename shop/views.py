d5ccd2793300c72bbc693647ab8d26f0b14d6374
Bravo604 <bravo60444@gmail.com>
2022-07-27 19:52:30 +0600

    real second hw

--- shop/views.py
+++ shop/views.py
@@ -16,23 +16,4 @@ def detail_item(request, item_id):
     context = {'item': item}
     return render(request, 'detail_item.html', context)
 
-#
-# def result(request, question_id):
-#     question = get_object_or_404(Question, id=question_id)
-#     context = {'question': question}
-#     return render(request, 'result.html', context)
-#
-#
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, id=question_id)
-#     choice = question.choice_set.get(id=request.POST['choice'])
-#     choice.votes += 1
-#     choice.save()
-#     return HttpResponseRedirect(reverse('result', args=(question.id, )))
-#
-#
-#
-#
-# def greetings(request):
-#     return HttpResponse('Добро пожаловать в наш магазин')
-#
+
